c0f05f77859a3f2a26ffbd6886a1814236f5b718
ABRILSAAC <felpe.toon@gmail.com>
2024-11-07 00:03:46 -0500

    Estamos enviando estos archivos de prueba para validar el github

---
+++ src/main/java/Energia/EnergiaHidroelectrica.java
@@ -0,0 +1,32 @@
+package Energia;
+
+
+public class EnergiaHidroelectrica extends Energia {
+    private double temperaturaSubterranea;
+
+    public Geotermica(String id, double potenciaMaxima, double temperaturaSubterranea) {
+        super(id, potenciaMaxima);
+        this.temperaturaSubterranea = temperaturaSubterranea;
+    }
+
+    // Getters y Setters
+    public double getTemperaturaSubterranea() {
+        return temperaturaSubterranea;
+    }
+
+    public void setTemperaturaSubterranea(double temperaturaSubterranea) {
+        this.temperaturaSubterranea = temperaturaSubterranea;
+    }
+
+    @Override
+    public void verificarEstado() {
+        if (temperaturaSubterranea < 100) {
+            setEnFuncionamiento(false);
+            System.out.println("La planta geotérmica " + getId() + " no tiene suficiente temperatura para operar.");
+        } else {
+            setEnFuncionamiento(true);
+            System.out.println("La planta geotérmica " + getId() + " está en funcionamiento.");
+        }
+    }
+}
+
